d93a12a2f8ba745dfade95404ed7ed44a3a95ae9
김재만 <96468321+jaemaning@users.noreply.github.com>
2022-01-29 16:58:03 +0900

    Update hash_01.py

--- hash_01.py
+++ hash_01.py
@@ -1,6 +1,7 @@
 # [문제 출처] - https://programmers.co.kr/learn/courses/30/lessons/42576
 # 완주하지 못한 선수 (hash table algorithm)
 
+
 def solution(participant, completion):
     answer = ''
     dic={}
@@ -16,7 +17,6 @@ def solution(participant, completion):
     answer = dic[temp]
     
     return answer
-
 
 
 ## 공백용 ##
