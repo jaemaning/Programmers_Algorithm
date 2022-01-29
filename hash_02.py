243b894152ab4dbd616fbe2c1e7eb083e84781e9
김재만 <96468321+jaemaning@users.noreply.github.com>
2022-01-29 16:51:30 +0900

    Create hash_02.py

---
+++ hash_02.py
@@ -0,0 +1,18 @@
+# 전화번호 목록 (hash table algorithm)
+
+def solution(phone_book):
+    answer = True
+    dic = {}
+    
+    for phone_num in phone_book:
+        dic[phone_num] = 1
+    
+    for phone_num in phone_book:
+        numbering = ""
+        for i in phone_num:
+            numbering += i
+            if numbering in dic and numbering != phone_num:
+                answer = False
+                break
+                
+    return answer
