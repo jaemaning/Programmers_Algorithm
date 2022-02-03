2cda5795c8175ebd708d0e6bde6bd2b814f4a19a
김재만 <96468321+jaemaning@users.noreply.github.com>
2022-02-03 23:01:44 +0900

    Update stackQueue_03.py
    제 방법과 비슷하지만 reverse 활용으로 popleft 사용 최소화로 리스트 맨뒷요소를 제거함으로써 리스트 재정렬을 안하는 속도 단축 
    + 마지막 트럭이 bridge에 들어감과 동시에 step에 bridge_length를 더해줌으로써 마지막 트럭 진입과 동시에 작업종료로 속도 단축

--- stackQueue_03.py
+++ stackQueue_03.py
@@ -26,3 +26,31 @@ def solution(bridge_length, weight, truck_weights):
   ## 공백용 ##
   
   # 5번 테스트 시간초과로 실패,,
+
+ ## 코드는 비슷하나 방법론 변경으로 속도단축에 있어서 압도적이길래,, 프로그래머스 익명의 고수님들의 코드를 빌려왔습니다.
+
+
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = deque(0 for _ in range(bridge_length))
+    total_weight = 0
+    step = 0
+    truck_weights.reverse()
+
+    while truck_weights:
+        total_weight -= bridge.popleft()
+        if total_weight + truck_weights[-1] > weight:
+            bridge.append(0)
+        else:
+            truck = truck_weights.pop()
+            bridge.append(truck)
+            total_weight += truck
+        step += 1
+
+    step += bridge_length
+
+    return step
+
+
+# 프로그래머스 임영준 , youjeongsue , 주익정 , SeungHyun Kim , 박홍균 외 5 명 풀이방법,,
