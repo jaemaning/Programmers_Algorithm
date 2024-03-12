0c36fbeec372ae2ad0d268efb1a1b35757b3ea06
jaemaning <lovesay00@naver.com>
2024-03-12 14:16:37 +0900

    programmers problem solve

---
+++ programmers/2024KAKAO_04.py
@@ -0,0 +1,53 @@
+
+def solution(coin, cards):
+    answer = 0
+    
+    n = len(cards)
+    target = n + 1
+    
+    hand = cards[:n//3]
+    wish = cards[n//3:n//3+2]
+    cards = cards[n//3+2:]
+    flag = 1
+    min_v = (n // 6) * 2 + 1
+    
+    while flag:
+        for i in range(len(hand)):
+            t = target - hand[i]
+            if t in hand:
+                print("??")
+                answer += 1
+                new_card = cards[:2]
+                cards = cards[2:]
+                wish = wish + new_card
+                hand.pop(i)
+                hand.pop(hand.index(t))
+                break
+                
+            elif t in wish:
+                if coin > 0:
+                    answer += 1
+                    coin -= 1
+                    new_card = cards[:2]
+                    cards = cards[2:]
+                    wish = wish + new_card
+                    hand.pop(i)
+                    wish.pop(wish.index(t))
+                    break
+        
+        else:
+            for j in range(len(wish)):
+                t = target - wish[j]
+                if t in wish and coin > 1:
+                    answer += 1
+                    coin -= 2
+                    wish.pop(j)
+                    wish.pop(wish.index(t))
+                    new_card = cards[:2]
+                    cards = cards[2:]
+                    wish = wish + new_card
+                    break
+            else:
+                flag = 0
+                        
+    return min(answer + 1, min_v)
