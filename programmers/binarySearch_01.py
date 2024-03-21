fac2cf696ea719ba7be97aa0f83ac64598a73ec7
jaemaning <lovesay00@naver.com>
2024-03-21 15:26:39 +0900

    programmers problem solve

---
+++ programmers/binarySearch_01.py
@@ -0,0 +1,21 @@
+# [문제 출처] - https://school.programmers.co.kr/learn/courses/30/lessons/43238
+# 입국심사 (binary search table algorithm)
+
+
+def solution(n, times):
+
+    s, e = 1, max(times)*n
+    while s <= e:
+        mid = (s+e) // 2
+        val = 0
+        for t in times:
+            val += mid // t
+            if val >= n:
+                break
+        
+        if val >= n:
+            e = mid-1
+        else:
+            s = mid+1
+    
+    return s
